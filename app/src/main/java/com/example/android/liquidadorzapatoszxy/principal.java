783fa9eeba829ce696b07a86626b11b2bb76700e
Carlos <carrieta6@cuc.edu.co>
2017-08-29 20:49:46 -0500

    Completo
    Commit

--- app/src/main/java/com/example/android/liquidadorzapatoszxy/principal.java
+++ app/src/main/java/com/example/android/liquidadorzapatoszxy/principal.java
@@ -1,13 +1,135 @@
 package com.example.android.liquidadorzapatoszxy;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class principal extends AppCompatActivity {
+
+    private TextView resultado;
+    private EditText cantidad;
+    private Resources resources;
+    private Spinner sexo;
+    private Spinner tipo;
+    private Spinner marca;
+    private String sex[];
+    private String type[];
+    private String brand[];
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        resultado = (TextView)findViewById(R.id.lblResultado);
+        cantidad = (EditText)findViewById(R.id.txtCantidad);
+        sexo = (Spinner)findViewById(R.id.spSexo);
+        tipo = (Spinner)findViewById(R.id.spTipo);
+        marca = (Spinner)findViewById(R.id.spMarca);
+        resources = this.getResources();
+        sex = resources.getStringArray(R.array.spsexo);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,sex);
+        sexo.setAdapter(adapter);
+
+        type = resources.getStringArray(R.array.sptipo);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,type);
+        tipo.setAdapter(adapter1);
+
+        brand = resources.getStringArray(R.array.spmarca);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,brand);
+        marca.setAdapter(adapter2);
+
     }
+
+    public boolean validar(){
+        if(cantidad.getText().toString().isEmpty() || cantidad.getText().toString().equals("0")){
+            cantidad.setError(resources.getString(R.string.mensaje_error_uno));
+            return false;
+        }
+
+        return true;
+    }
+
+    public void calcular(View v){
+        int sex1;
+        int tip1;
+        int marc1;
+        int res=0;
+        int cant;
+
+
+        resultado.setText("");
+        if(validar()){
+
+            sex1 = sexo.getSelectedItemPosition();
+            tip1 = tipo.getSelectedItemPosition();
+            marc1 = marca.getSelectedItemPosition();
+            cant = Integer.parseInt(cantidad.getText().toString());
+
+
+            if(sex1==0 && tip1==0 && marc1==0){
+                res = 120000*cant;
+            }else{
+                if(sex1==0 && tip1==0 && marc1==1){
+                    res = 140000*cant;
+                }else{
+                    if(sex1==0 && tip1==0 && marc1==2){
+                        res = 130000*cant;
+                    }else{
+                        if(sex1==0 && tip1==1 && marc1==0){
+                            res = 50000*cant;
+                        }else{
+                            if(sex1==0 && tip1==1 && marc1==1){
+                              res = 80000*cant;
+                            }else{
+                                if(sex1==0 && tip1==1 && marc1==2){
+                                    res = 100000*cant;
+                                }else{
+                                    if(sex1==1 && tip1==0 && marc1==0){
+                                        res = 100000*cant;
+                                    }else{
+                                        if(sex1==1 && tip1==0 && marc1==1){
+                                            res = 130000*cant;
+                                        }else{
+                                            if(sex1==1 && tip1==0 && marc1==2){
+                                                res = 110000*cant;
+                                            }else{
+                                                if(sex1==1 && tip1==1 && marc1==0){
+                                                    res = 60000*cant;
+                                                }else{
+                                                    if(sex1==1 && tip1==1 && marc1==1){
+                                                        res = 70000*cant;
+                                                    }else{
+                                                        if(sex1==1 && tip1==1 && marc1==2){
+                                                            res = 120000*cant;
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                        }
+
+                    }
+                }
+
+            }
+
+            resultado.setText(String.valueOf(res));
+        }
+
+    }
+
 }
