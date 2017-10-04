9f46c6a8915adcd023872eeee57cf6823652ec95
Carlos <carrieta6@cuc.edu.co>
2017-10-03 20:34:08 -0500

    all test done
    all test done

--- app/src/test/java/com/example/android/liquidadorzapatoszxy/ExampleUnitTest.java
+++ app/src/test/java/com/example/android/liquidadorzapatoszxy/ExampleUnitTest.java
@@ -32,7 +32,249 @@ public class ExampleUnitTest {
 
         actual = Metodos.total(sex1,tip1,marc1,cantidad);
         esperado = 120000;
-        
+
+        assertEquals(actual,esperado,0);
+
+    }
+
+    @Test
+    public void probarCombinacion2(){
+        int cantidad;
+        int sex1;
+        int tip1;
+        int marc1;
+        int res;
+        int actual;
+        int esperado;
+
+        cantidad =1;
+        sex1 = 0;
+        tip1 = 0;
+        marc1 = 1;
+
+        actual = Metodos.total(sex1,tip1,marc1,cantidad);
+        esperado = 140000;
+
+        assertEquals(actual,esperado,0);
+
+    }
+
+    @Test
+    public void probarCombinacion3(){
+        int cantidad;
+        int sex1;
+        int tip1;
+        int marc1;
+        int res;
+        int actual;
+        int esperado;
+
+        cantidad =1;
+        sex1 = 0;
+        tip1 = 0;
+        marc1 = 2;
+
+        actual = Metodos.total(sex1,tip1,marc1,cantidad);
+        esperado = 130000;
+
+        assertEquals(actual,esperado,0);
+
+    }
+
+    @Test
+    public void probarCombinacion4(){
+        int cantidad;
+        int sex1;
+        int tip1;
+        int marc1;
+        int res;
+        int actual;
+        int esperado;
+
+        cantidad =1;
+        sex1 = 0;
+        tip1 = 1;
+        marc1 = 0;
+
+        actual = Metodos.total(sex1,tip1,marc1,cantidad);
+        esperado = 50000;
+
+        assertEquals(actual,esperado,0);
+
+    }
+
+    @Test
+    public void probarCombinacion5(){
+        int cantidad;
+        int sex1;
+        int tip1;
+        int marc1;
+        int res;
+        int actual;
+        int esperado;
+
+        cantidad =1;
+        sex1 = 0;
+        tip1 = 1;
+        marc1 = 1;
+
+        actual = Metodos.total(sex1,tip1,marc1,cantidad);
+        esperado = 80000;
+
+        assertEquals(actual,esperado,0);
+
+    }
+
+    @Test
+    public void probarCombinacion6(){
+        int cantidad;
+        int sex1;
+        int tip1;
+        int marc1;
+        int res;
+        int actual;
+        int esperado;
+
+        cantidad =1;
+        sex1 = 0;
+        tip1 = 1;
+        marc1 = 2;
+
+        actual = Metodos.total(sex1,tip1,marc1,cantidad);
+        esperado = 100000;
+
+        assertEquals(actual,esperado,0);
+
+    }
+
+    @Test
+    public void probarCombinacion7(){
+        int cantidad;
+        int sex1;
+        int tip1;
+        int marc1;
+        int res;
+        int actual;
+        int esperado;
+
+        cantidad =1;
+        sex1 = 1;
+        tip1 = 0;
+        marc1 = 0;
+
+        actual = Metodos.total(sex1,tip1,marc1,cantidad);
+        esperado = 100000;
+
+        assertEquals(actual,esperado,0);
+
+    }
+
+    @Test
+    public void probarCombinacion8(){
+        int cantidad;
+        int sex1;
+        int tip1;
+        int marc1;
+        int res;
+        int actual;
+        int esperado;
+
+        cantidad =1;
+        sex1 = 1;
+        tip1 = 0;
+        marc1 = 1;
+
+        actual = Metodos.total(sex1,tip1,marc1,cantidad);
+        esperado = 130000;
+
+        assertEquals(actual,esperado,0);
+
+    }
+
+    @Test
+    public void probarCombinacion9(){
+        int cantidad;
+        int sex1;
+        int tip1;
+        int marc1;
+        int res;
+        int actual;
+        int esperado;
+
+        cantidad =1;
+        sex1 = 1;
+        tip1 = 0;
+        marc1 = 2;
+
+        actual = Metodos.total(sex1,tip1,marc1,cantidad);
+        esperado = 110000;
+
+        assertEquals(actual,esperado,0);
+
+    }
+
+    @Test
+    public void probarCombinacion10(){
+        int cantidad;
+        int sex1;
+        int tip1;
+        int marc1;
+        int res;
+        int actual;
+        int esperado;
+
+        cantidad =1;
+        sex1 = 1;
+        tip1 = 1;
+        marc1 = 0;
+
+        actual = Metodos.total(sex1,tip1,marc1,cantidad);
+        esperado = 60000;
+
+        assertEquals(actual,esperado,0);
+
+    }
+
+    @Test
+    public void probarCombinacion11(){
+        int cantidad;
+        int sex1;
+        int tip1;
+        int marc1;
+        int res;
+        int actual;
+        int esperado;
+
+        cantidad =1;
+        sex1 = 1;
+        tip1 = 1;
+        marc1 = 1;
+
+        actual = Metodos.total(sex1,tip1,marc1,cantidad);
+        esperado = 70000;
+
+        assertEquals(actual,esperado,0);
+
+    }
+
+    @Test
+    public void probarCombinacion12(){
+        int cantidad;
+        int sex1;
+        int tip1;
+        int marc1;
+        int res;
+        int actual;
+        int esperado;
+
+        cantidad =1;
+        sex1 = 1;
+        tip1 = 1;
+        marc1 = 2;
+
+        actual = Metodos.total(sex1,tip1,marc1,cantidad);
+        esperado = 120000;
+
         assertEquals(actual,esperado,0);
 
     }
